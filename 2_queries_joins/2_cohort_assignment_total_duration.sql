148c18d0683b8deb6c911e0ecde948bdc2182186
John Webster <johnrwebs@gmail.com>
2019-08-19 19:24:19 +0000

    added grouped queries exercise

--- 2_queries_joins/2_cohort_assignment_total_duration.sql
+++ 2_queries_joins/2_cohort_assignment_total_duration.sql
@@ -1,5 +1,5 @@
-SELECT SUM(assignemnt_submissions.duration) as total_duration
+SELECT SUM(assignment_submissions.duration) as total_duration
   FROM assignment_submissions
     JOIN students ON students.id = student_id
     JOIN cohorts ON cohorts.id = cohort_id
-  WHERE cohorts.name = "FEB12"; 
+  WHERE cohorts.name = "FEB12";
